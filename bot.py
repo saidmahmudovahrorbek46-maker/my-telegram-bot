263c291afd2de9544641a3031438e9048aa493df
saidmahmudovahrorbek46-maker <saidmahmudovahrorbek46@gmail.com>
2026-06-15 16:56:08 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import telebot, random, os, json, datetime
 
 # ⚠️ Telegram Bot Token
-bot = telebot.TeleBot("8992035812:AAHR6ixhqDqZ9nzW_kynOu_Jluy0cvpSdd8")
+bot = telebot.TeleBot("8992035812:AAE0rGeTagE0CaxnjkiSyV3ZdgAgH6vlsTw")
 ADMIN_ID = 7523074495  
 user_sessions = {}
 DB_FILE = "bot_users.json"
